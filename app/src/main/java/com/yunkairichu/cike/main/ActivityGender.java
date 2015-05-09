872dda5dda36a28b067996c4977949a280ca9268
jasonxbliu <liuxiaobo87@foxmail.com>
2015-05-09 18:42:13 +0800

    add register page file - gender selection

---
+++ app/src/main/java/com/yunkairichu/cike/main/ActivityGender.java
@@ -0,0 +1,15 @@
+package com.yunkairichu.cike.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by liuxiaobo on 15/5/9.
+ */
+public class ActivityGender extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gender);
+    }
+}
