a5d885adcb4eb17c5102a6caace894352db69bd2
jasonxbliu <liuxiaobo87@foxmail.com>
2015-05-09 20:52:55 +0800

    add role selection

--- app/src/main/java/com/yunkairichu/cike/main/ActivityRole.java
+++ app/src/main/java/com/yunkairichu/cike/main/ActivityRole.java
@@ -2,16 +2,14 @@ package com.yunkairichu.cike.main;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
 
 /**
  * Created by liuxiaobo on 15/5/9.
  */
-public class ActivityGender extends Activity {
+public class ActivityRole extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_gender);
+        setContentView(R.layout.activity_role);
     }
 }
