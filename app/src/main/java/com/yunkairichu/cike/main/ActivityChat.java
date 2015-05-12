783eb7008d769d4cb64cc1502360e86f23212da9
jasonxbliu <liuxiaobo87@foxmail.com>
2015-05-12 21:15:53 +0800

    增加聊天界面布局文件，写好了框架，还需完善。主要差聊天listview和举报与拍照菜单的实现

---
+++ app/src/main/java/com/yunkairichu/cike/main/ActivityChat.java
@@ -0,0 +1,110 @@
+package com.yunkairichu.cike.main;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.text.ClipboardManager;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.yunkairichu.cike.widget.PasteEditText;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by liuxiaobo on 15/5/12.
+ */
+public class ActivityChat extends Activity {
+
+    private static final int REQUEST_CODE_EMPTY_HISTORY = 2;
+    public static final int REQUEST_CODE_CONTEXT_MENU = 3;
+    private static final int REQUEST_CODE_MAP = 4;
+    public static final int REQUEST_CODE_TEXT = 5;
+    public static final int REQUEST_CODE_VOICE = 6;
+    public static final int REQUEST_CODE_PICTURE = 7;
+    public static final int REQUEST_CODE_LOCATION = 8;
+    public static final int REQUEST_CODE_NET_DISK = 9;
+    public static final int REQUEST_CODE_FILE = 10;
+    public static final int REQUEST_CODE_COPY_AND_PASTE = 11;
+    public static final int REQUEST_CODE_PICK_VIDEO = 12;
+    public static final int REQUEST_CODE_DOWNLOAD_VIDEO = 13;
+    public static final int REQUEST_CODE_VIDEO = 14;
+    public static final int REQUEST_CODE_DOWNLOAD_VOICE = 15;
+    public static final int REQUEST_CODE_SELECT_USER_CARD = 16;
+    public static final int REQUEST_CODE_SEND_USER_CARD = 17;
+    public static final int REQUEST_CODE_CAMERA = 18;
+    public static final int REQUEST_CODE_LOCAL = 19;
+    public static final int REQUEST_CODE_CLICK_DESTORY_IMG = 20;
+    public static final int REQUEST_CODE_GROUP_DETAIL = 21;
+    public static final int REQUEST_CODE_SELECT_VIDEO = 23;
+    public static final int REQUEST_CODE_SELECT_FILE = 24;
+    public static final int REQUEST_CODE_ADD_TO_BLACKLIST = 25;
+
+    public static final int RESULT_CODE_COPY = 1;
+    public static final int RESULT_CODE_DELETE = 2;
+    public static final int RESULT_CODE_FORWARD = 3;
+    public static final int RESULT_CODE_OPEN = 4;
+    public static final int RESULT_CODE_DWONLOAD = 5;
+    public static final int RESULT_CODE_TO_CLOUD = 6;
+    public static final int RESULT_CODE_EXIT_GROUP = 7;
+
+    public static final int CHATTYPE_SINGLE = 1;
+    public static final int CHATTYPE_GROUP = 2;
+
+    public static final String COPY_IMAGE = "EASEMOBIMG";
+    private View recordingContainer;
+    private ImageView micImage;
+    private TextView recordingHint;
+    private ListView listView;
+    private PasteEditText mEditTextContent;
+    private View buttonSetModeKeyboard;
+    private View buttonSetModeVoice;
+    private View buttonSend;
+    private View buttonPressToSpeak;
+    // private ViewPager expressionViewpager;
+    private LinearLayout emojiIconContainer;
+    private LinearLayout btnContainer;
+    private ImageView locationImgview;
+    private View more;
+    private int position;
+    private ClipboardManager clipboard;
+    private ViewPager expressionViewpager;
+    private InputMethodManager manager;
+    private List<String> reslist;
+    private Drawable[] micImages;
+    private int chatType;
+//    private EMConversation conversation;
+//    public static ChatActivity activityInstance = null;
+    // 给谁发送消息
+    private String toChatUsername;
+//    private VoiceRecorder voiceRecorder;
+//    private MessageAdapter adapter;
+    private File cameraFile;
+    static int resendPos;
+
+//    private GroupListener groupListener;
+
+    private ImageView iv_emoticons_normal;
+    private ImageView iv_emoticons_checked;
+    private RelativeLayout edittext_layout;
+    private ProgressBar loadmorePB;
+    private boolean isloading;
+    private final int pagesize = 20;
+    private boolean haveMoreData = true;
+    private Button btnMore;
+    public String playMsgId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
